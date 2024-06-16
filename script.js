92a5294be7e7c648fc7eb611fb652adace6f0528
Ingini <130362544+InIngini@users.noreply.github.com>
2024-06-16 13:56:01 +0500

    осталось только бд

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     setCanvasBackground();
+    addToHistory();
 });
 // Обработка события изменения размера окна
 window.addEventListener("resize", () => {
@@ -52,33 +53,74 @@ var inputH = document.getElementById('H');
 var btn = document.getElementById('wh');
 
 const drawOffice = () => {
+
     // Получение значений ширины и высоты из элементов ввода
     const width = parseInt(inputW.value);
     const height = parseInt(inputH.value);
 
     // Проверка, не превышают ли размеры 100
     if (width > 100 || height > 100) {
-        alert('У вас не может быть такой большой офис!');
+        alert('У вас не может быть такой большой офис! Максимальный размер: 100x100');
         return;
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    tableCoordinates.slice(0);
+    lastTableNumber=0;
+
     if (width<=50&&height<=50)
     {
         ctx.strokeRect(50, 50, width*15, height*15);
+        tableCoordinates.push({
+            number: 0,
+            x1: 50,
+            y1: 50,
+            x2: 50+width*15,
+            y2: 50+height*15
+          });
     }
     else if ((width<=70&&width>50)&&height<=50){
         ctx.strokeRect(50, 50, width*13, height*13);
+        tableCoordinates.push({
+            number: 0,
+            x1: 50,
+            y1: 50,
+            x2: 50+width*13,
+            y2: 50+height*13
+          });
     }
     else if ((width<=100&&width>70)&&height<=50){
         ctx.strokeRect(50, 50, width*10, height*10);
+        tableCoordinates.push({
+            number: 0,
+            x1: 50,
+            y1: 50,
+            x2: 50+width*10,
+            y2: 50+height*10
+          });
     }
     else if (width<=70&&height<=70){
         ctx.strokeRect(50, 50, width*10, height*10);
+        tableCoordinates.push({
+            number: 0,
+            x1: 50,
+            y1: 50,
+            x2: 50+width*10,
+            y2: 50+height*10
+          });
     }
     else if (width<=100&&height<=100){
         ctx.strokeRect(50, 50, width*7, height*7);
+        tableCoordinates.push({
+            number: 0,
+            x1: 50,
+            y1: 50,
+            x2: 50+width*7,
+            y2: 50+height*7
+          });
     }
+    canvas.focus();
+    addToHistory();
 };
 
 // Слушатель события щелчка для кнопки
@@ -407,15 +449,99 @@ const drawLadder = (e) => {
     ctx.stroke(); // Отрисовка прямоугольника
 
     // Рисование палок лестницы
-    for (let i = 1; i < (e.offsetY-prevMouseY)/20; i++) {
+    for (let i = 1; i < (e.offsetY-prevMouseY)/10; i++) {
         ctx.beginPath(); // Создание нового пути для рисования линии
-        ctx.moveTo(prevMouseX, prevMouseY + 20 * i); // Перемещение линии в положение указателя мыши
-        ctx.lineTo(e.offsetX, prevMouseY + 20 * i); // Создание линии в соответствии с положением указателя мыши
+        ctx.moveTo(prevMouseX, prevMouseY + 10 * i); // Перемещение линии в положение указателя мыши
+        ctx.lineTo(e.offsetX, prevMouseY + 10 * i); // Создание линии в соответствии с положением указателя мыши
         ctx.stroke(); // Отрисовка линии
     }
 };
 /////////ктрл+зт, ктрл+у////////////
 // Стек действий (для отмены)
+// Массив для хранения истории изменений
+let history = [];
+let historyIndex = -1;
+
+function removAfterToHistory(){
+    // Получаем текущий индекс в истории
+    let currentIndex = historyIndex;
+
+    // Если есть значения после текущего, удаляем их из истории
+    if (currentIndex < history.length - 1) {
+        history = history.slice(0, currentIndex + 1);
+    }
+}
+
+// Функция для добавления состояния в историю
+function addToHistory() {
+  // Сохраняем текущее состояние canvas
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  history.push(imageData);
+  historyIndex++;
+}
+
+// Функция для отмены изменений (Ctrl+Z)
+function undo() {
+  if (historyIndex > -1) {
+    historyIndex--;
+    ctx.putImageData(history[historyIndex], 0, 0);
+    
+  }
+}
+
+// Функция для повтора изменений (Ctrl+Y)
+function redo() {
+  if (historyIndex < history.length - 1) {
+    historyIndex++;
+    ctx.putImageData(history[historyIndex], 0, 0);
+  }
+}
+
+
+
+// Состояние нажатия клавиш
+let isCtrlPressed = false;
+let isZPressed = false;
+let isYPressed = false;
+
+// Глобальный обработчик событий клавиатуры
+document.addEventListener('keydown', (event) => {
+    if(event.ctrlKey||event.key.toLowerCase() === 'z'|| event.key.toLowerCase() === 'я'||event.key.toLowerCase() === 'y'|| event.key.toLowerCase() === 'н')
+    {  // Проверяем состояние нажатия клавиш
+        if (event.ctrlKey) {
+            isCtrlPressed = true;
+        }
+        if (event.key.toLowerCase() === 'z'|| event.key.toLowerCase() === 'я') {
+            isZPressed = true;
+        }
+        if (event.key.toLowerCase() === 'y'|| event.key.toLowerCase() === 'н') {
+            isYPressed = true;
+        }
+    }
+    else
+    {
+        isCtrlPressed = false;
+        isZPressed = false;
+        isYPressed = false;
+    }
+
+  // Если нажаты Ctrl + Z, отменяем стандартное поведение
+  if (isCtrlPressed && isZPressed) {
+    event.preventDefault();
+    undo(event.target);
+    isCtrlPressed = false;
+    isZPressed = false;
+    isYPressed = false;
+  }
+  // Если нажаты Ctrl + Y, отменяем стандартное поведение
+  else if (isCtrlPressed && isYPressed) {
+    event.preventDefault();
+    redo(event.target);
+    isCtrlPressed = false;
+    isZPressed = false;
+    isYPressed = false;
+  }
+});
 
 //////////////Маусдаун, маусмув и маусап/////////////////
 // Начало рисования
@@ -429,7 +555,6 @@ const startDraw = (e) => {
     ctx.fillStyle = selectedColor; // Передача selectedColor как стиля заливки
     // Копирование данных холста и передача в качестве значения snapshot.. это предотвращает перетаскивание изображения
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    
 
     if (selectedTool === "text"){
         
@@ -467,6 +592,8 @@ const drawing = (e) => {
         drawTriangle(e);
     } else if (selectedTool === "line"){
         drawLine(e);
+    } else if (selectedTool === "room"){
+        drawRoom(e);
     } else if (selectedTool === "table"){
         drawTable(e);
     } else if (selectedTool === "m"){
@@ -524,6 +651,8 @@ const tableCoordinates = [];
 
 
 function handleMouseUp(e) {
+    removAfterToHistory();
+    addToHistory();
     if (selectedTool === "table") {
 
       // Сохранение координат стола в массиве
@@ -534,6 +663,7 @@ function handleMouseUp(e) {
         x2: e.offsetX,
         y2: e.offsetY
       });
+      //alert(prevMouseX+" "+prevMouseY+" "+e.offsetX+" "+e.offsetY);
     }
   
     isDrawing = false;
